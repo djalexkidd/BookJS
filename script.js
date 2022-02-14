339c076e34810ab93b7852437798648b8f5ab08d
djalexkidd <alexpyr@outlook.fr>
2022-02-14 15:44:36 +0100

    OMG IT WORKS !

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const inputField = document.querySelectorAll("input")
 const sendForm = document.querySelector("form")
 const resetAll = document.querySelector(".button-erase")
 const addStatus = document.querySelector(".add-success")
+const listItems = document.querySelector('.list-items');
+const noBook = document.querySelector('.no-book');
 
 const bookTitle = document.querySelector(".book-title")
+const bookAuthor = document.querySelector(".book-author")
+const bookPages = document.querySelector(".book-pages")
 
 sendForm.addEventListener('submit', submitBook)
 
@@ -38,19 +42,71 @@ resetAll.addEventListener('click', (e) => {
 function submitBook(e) {
     e.preventDefault() // N'actualise pas la page
     addStatus.classList.toggle("hidden") // Affiche le texte pour dire que le livre à été enregistré
+
+    const timestamp = Date.now();
+    bookList[timestamp] = {
+        title: bookTitle.value,
+        author: bookAuthor.value,
+        pages: bookPages.value
+    }
+
     for (let i = 0; i < inputField.length; i++) {
         inputField[i].value = "" // Efface le formulaire
     }
 
-    const timestamp = Date.now();
-    bookList[timestamp] = {
-        title: bookTitle.value
-    }
+    createHTML(bookList[timestamp], timestamp);
     saveObj();
 
     setTimeout(() => {addStatus.classList.toggle("hidden")}, 5000) // Attendre 5 secondes pour cacher le texte
 }
 
+// Boucler sur l'objet
+function loadHTML() {
+    if(!window.localStorage.getItem('data')) return
+    const data = JSON.parse(window.localStorage.getItem('data'))
+    bookList = data;
+    Object.keys(bookList).map(key => createHTML(bookList[key], key));
+    noBook.classList.toggle("hidden") // Affiche le texte pour dire que le livre à été enregistré
+    checkBookNumber()
+}
+
+window.addEventListener('load', loadHTML);
+
+function createHTML(objet, key) {
+    if(!objet.title) return;
+    const html = `
+            <span>${objet.title} par ${objet.author} (${objet.pages} pages)</span>
+            <button name="trash" class="trash">🗑️</button>
+    `
+
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.setAttribute('data-key', key);
+    li.innerHTML = html;
+    listItems.insertBefore(li, listItems.firstChild);
+
+    li.children.trash.onclick = toBin;
+
+    checkBookNumber()
+}
+
+function toBin() {
+    this.parentNode.remove();
+    const key = this.parentNode.getAttribute('data-key');
+    delete bookList[key];
+    saveObj();
+    checkBookNumber()
+}
+
 function saveObj() {
     window.localStorage.setItem('data', JSON.stringify(bookList));
+}
+
+function checkBookNumber() {
+    if (Object.keys(bookList).length === 0) {
+        noBook.classList.remove("hidden")
+    }
+    else {
+        noBook.classList.add("hidden")
+    }
 }
