85645985f6979095d5ecf72b581001d3b643f3f5
djalexkidd <alexpyr@outlook.fr>
2022-02-15 11:43:35 +0100

    Tester c'est important

--- themes.js
+++ themes.js
@@ -1,12 +1,16 @@
 const themeSwitch = document.querySelector(".themeSwitch")
 const bodyTheme = document.querySelector("body")
 const linkTheme = document.querySelectorAll("a")
+const items = document.querySelectorAll(".list-items")
 
 themeSwitch.addEventListener('click', switchTheme)
 
 function switchTheme() {
-    bodyTheme.classList.toggle("darkmode") // Si non, cache le texte
+    bodyTheme.classList.toggle("darkmode")
     for (let i = 0; i < linkTheme.length; i++) {
-        linkTheme[i].classList.toggle("darkmode") // Si non, cache le texte
+        linkTheme[i].classList.toggle("darkmode")
+    }
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.toggle("darkmode")
     }
 }
