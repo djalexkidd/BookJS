cd58d2f37018ba91076ece05eeb879232035563b
djalexkidd <alexpyr@outlook.fr>
2022-02-15 11:58:35 +0100

    darkmode-img

--- themes.js
+++ themes.js
@@ -2,11 +2,13 @@ const themeSwitch = document.querySelector(".themeSwitch")
 const bodyTheme = document.querySelector("body")
 const linkTheme = document.querySelectorAll("a")
 const items = document.querySelectorAll(".list-items")
+const imgTheme = document.querySelector("img")
 
 themeSwitch.addEventListener('click', switchTheme)
 
 function switchTheme() {
     bodyTheme.classList.toggle("darkmode")
+    imgTheme.classList.toggle("darkmode-img")
     for (let i = 0; i < linkTheme.length; i++) {
         linkTheme[i].classList.toggle("darkmode")
     }
